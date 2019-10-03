ce1ba73e681e98e71e69d6c4042dea2414274bd7
Nicolas Le Gall <contact@nlegall.fr>
2019-10-03 11:19:16 +0200

    [Python] Add get ipinfo
    Signed-off-by: Nicolas Le Gall <contact@nlegall.fr>

--- python.py
+++ python.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 import requests
+import json
 
 r = requests.get('https://api.floodipdb.info/totalIp')
+print(r.json())
+
+payload = {'ip': '45.227.253.214'}
+r = requests.post('https://api.floodipdb.info/ip/info', json=payload)
 print(r.json())
